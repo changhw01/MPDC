0eff60c59c7b15f84a8a6dd9b19130904a98b071
Huang-Wei Chang <chang.huangwei.01@gmail.com>
2012-10-18 13:14:43 -0700

    new file:   nbr_clustering.m 	modified:   pclustering_fullbasis.m

---
+++ nbr_clustering.m
@@ -0,0 +1,15 @@
+function [C_cell, n_clst] = nbr_clustering(distM, s, I_want)
+
+% Compute the neighborhood clustering using a scale parameter
+% This is also the Rips complex
+
+if nargin < 3
+    I_want = 1:length(distM);
+end
+
+[C, n_clst] = graphconncomp(sparse(distM(I_want,I_want)<=s),'Weak',true);
+
+C_cell = cell(n_clst,1);
+for i = 1:n_clst
+    C_cell{i} = I_want((C==i));
+end
